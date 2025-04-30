aeb5a429aacb7abc96c477caeb438484b44816c2
Prashanth Naroju <prashanth.naro@gmail.com>
2025-04-30 11:46:12 +0530

    design and code the register page

---
+++ src/app/(...auth)/register/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import { RegisterForm } from '@/components/authentication/RegisterForm';
+
+const page = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+    <div className="w-full max-w-sm">
+      <RegisterForm />
+    </div>
+  </div>
+  )
+}
+
+export default page
