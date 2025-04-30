92bd75fa885adb172a3dd67bb78191de02f5cc97
Prashanth Naroju <prashanth.naro@gmail.com>
2025-04-30 12:26:08 +0530

    install auth.js for building authentication system

--- src/app/(...auth)/register/page.tsx
+++ src/app/(...auth)/register/page.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { RegisterForm } from '@/components/authentication/RegisterForm';
+import React from "react";
+import { RegisterForm } from "@/components/authentication/RegisterForm";
 
 const page = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-    <div className="w-full max-w-sm">
-      <RegisterForm />
+      <div className="w-full max-w-sm">
+        <RegisterForm />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
