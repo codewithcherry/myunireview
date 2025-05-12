a5779bedf6d158035e429dcfb80f34bbba0558bb
Prashanth Naroju <prashanth.naro@gmail.com>
2025-05-12 13:41:09 +0530

    update the searchBar for reusable component

--- src/app/find-uni/page.tsx
+++ src/app/find-uni/page.tsx
@@ -1,8 +1,13 @@
+import SearchBar from '@/components/search/SearchBar'
 import React from 'react'
 
 const page:React.FC = () => {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full h-screen bg-gray-50 dark:bg-zinc-900'>
+      <div className='max-w-6xl mx-auto py-10'>
+        <SearchBar />
+      </div>
+
       <div className='mx-auto '>
         <h1 className='text-2xl text-center text-indigo-400 animate-pulse mt-12'>
             Coming Soon
