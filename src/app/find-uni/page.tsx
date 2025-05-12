6b707d1498580ebc29214bf61350075a22caf739
Prashanth Naroju <prashanth.naro@gmail.com>
2025-05-12 10:32:20 +0530

    design and add coming soon placeholder for find uni page

---
+++ src/app/find-uni/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page:React.FC = () => {
+  return (
+    <div className='w-full h-screen'>
+      <div className='mx-auto '>
+        <h1 className='text-2xl text-center text-indigo-400 animate-pulse mt-12'>
+            Coming Soon
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
